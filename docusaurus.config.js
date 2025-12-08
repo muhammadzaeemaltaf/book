6309b21eb03d2dba8c2190d647857b1dc69e7473
Muhammad Zaeem Altaf <165184980+muhammadzaeemaltaf@users.noreply.github.com>
2025-12-09 02:01:28 +0500

    Update site URL in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://muhammadzaeemaltaf.github.io",
+  url: "https://humanoid-robotics-textbook-eight.vercel.app/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub Pages deployment, it's usually '/<orgName>/<repoName>/'
   baseUrl: isGithubPages ? "/book/" : "/",
